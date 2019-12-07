30e73a070abddbdb4ca701e27237d43ef76352da
yiduo <yiduo_huang@berkeley.edu>
2019-12-07 15:29:10 -0800

    lowess for speed ts

--- background_tse.py
+++ background_tse.py
@@ -4,24 +4,101 @@ import pandas as pd
 from sqlalchemy import create_engine
 import matplotlib.pyplot as plt
 
-xsh_s2n = geopandas.read_file('data_car/data/西三环南向北/西三环南向北.SHP', encoding='gbk')
-speed_record = pd.read_excel('data_car/data/link05m_lv5_20180601_西三环南向北.xlsx')
-speed_record_37592 = speed_record.loc[speed_record['linkid'] == 37592]
+holiday_list = [2,3,9,10,16,17,18,23,24,30]
+holiday_list = pd.DataFrame({'day':holiday_list})
 
+xsh_s2n = geopandas.read_file('data_car/data/西三环南向北/西三环南向北.SHP', encoding='gbk')
 
 count_start_end = pd.read_csv('count_start_end.csv', encoding='utf-8')
 stop_pair=['公主坟南', '六里桥北里']
 count_start_end_local = count_start_end.loc[count_start_end['NAME_y'].isin(stop_pair)]
-linenum_list = count_start_end_local['linenum'].unique()
+busstation_shp = geopandas.read_file(
+    'beijing_data/2014版北京路网_接点_六里桥区-2018/北京局部区域公交相关数据/北京局部区域公交相关数据/02 区域内公交站点GIS信息/station.shp', encoding='gbk')
+busstation_shp['STATION_ID'] = pd.to_numeric(busstation_shp['STATION_ID'])
+busstation_shp = busstation_shp.loc[busstation_shp['STATION_ID'].isin(count_start_end_local['STATION_ID'])]
+stop_set = busstation_shp['geometry'].unary_union.envelope
+maxy = max(stop_set.exterior.xy[1])
+miny = min(stop_set.exterior.xy[1])
+length = maxy-miny
+find_y_max = lambda x: max(x.coords.xy[1])
+find_y_min = lambda x: min(x.coords.xy[1])
+xsh_s2n['max_y'] = xsh_s2n['geometry'].apply(find_y_max)
+xsh_s2n['min_y'] = xsh_s2n['geometry'].apply(find_y_min)
+xsh_s2n.sort_values('max_y',inplace=True)
+xsh_s2n.reset_index(inplace=True,drop=True)
+
+xsh_s2n1 = xsh_s2n.loc[(xsh_s2n['max_y']>miny)&(xsh_s2n['min_y']<maxy)]
+xsh_s2n2 = xsh_s2n.loc[xsh_s2n1.index.min()-2:xsh_s2n1.index.max()+2]
+base = xsh_s2n1.plot(color='red')
+busstation_shp.plot(ax=base)
+xsh_s2n.plot(alpha=0.3,ax=base)
+xsh_s2n2.plot(alpha=0.5,ax=base,color='green')
 
 
+speed_record0 = pd.read_excel('data_car/data/link05m_lv5_20180601_西三环南向北.xlsx')
+speed_record0 = speed_record0.loc[speed_record0['linkid'].isin(xsh_s2n2.LinkID)]
+print('file 0 loaded')
+speed_record1 = pd.read_excel('data_car/data/link05m_lv5_20180602_20180607_西三环南向北.xlsx')
+speed_record1 = speed_record1.loc[speed_record1['linkid'].isin(xsh_s2n2.LinkID)]
+print('file 1 loaded')
+speed_record2 = pd.read_excel('data_car/data/link05m_lv5_20180608_20180614_西三环南向北.xlsx')
+speed_record2 = speed_record2.loc[speed_record2['linkid'].isin(xsh_s2n2.LinkID)]
+print('file 2 loaded')
+speed_record3 = pd.read_csv('data_car/data/link05m_lv5_20180615_20180630_西三环南向北.csv')
+speed_record3 = speed_record3.loc[speed_record3['linkid'].isin(xsh_s2n2.LinkID)]
+print('file 3 loaded')
+speed_record = pd.concat([speed_record0,speed_record1,speed_record2,speed_record3])
+speed_record['time'] = pd.to_datetime(speed_record['dt'].astype(str)+' '+speed_record['tm'].astype(str))
+speed_record=speed_record.drop(['dt','tm'],axis=1)
+speed_record = speed_record.loc[speed_record['time'].dt.time>pd.to_datetime('05:00:00').time()]
+speed_record = speed_record.loc[speed_record['time'].dt.time<pd.to_datetime('23:59:59').time()]
 
+
+speed_record['tm'] = speed_record['time'].dt.time.astype(str)
+speed_record['tm'] = speed_record['tm'].str[0].astype(int)*10*3600 + speed_record['tm'].str[1].astype(int)*3600 +speed_record['tm'].str[3].astype(int)*10*60+speed_record['tm'].str[4].astype(int)*60
+
+speed_record['base_pattern'] = 0
+holiday_record = speed_record.loc[speed_record['time'].dt.day.isin(holiday_list['day'])]
+workday_record = speed_record.loc[~(speed_record['time'].dt.day.isin(holiday_list['day']))]
+
+from statsmodels.nonparametric.smoothers_lowess import lowess
+def one_link_analysis(record, linkid=35972):
+    speed_link = record.loc[record['linkid']==linkid]
+
+    #plt.figure()
+    #for day in speed_link['time'].dt.date.unique():
+    #    speed_day = speed_link.loc[speed_link['time'].dt.date==day]
+    #    plt.scatter(speed_day['tm'], speed_day['speed'])
+    x = speed_link['tm'].values
+    y = speed_link['speed']
+    new = lowess(y,x,frac=0.15)
+
+
+    return new
+
+plt.figure()
+for ind, linkid in enumerate(xsh_s2n2.LinkID.values):
+    lowess_result = one_link_analysis(holiday_record, linkid)
+    plt.plot(lowess_result[:,0],lowess_result[:,1], label='sequence={0},linkid={1}'.format(ind,linkid))
+plt.legend()
+plt.xlabel('time(s)')
+plt.ylabel('lowess speed')
+plt.title('holiday base pattern')
+
+plt.figure()
+for ind, linkid in enumerate(xsh_s2n2.LinkID.values):
+    lowess_result = one_link_analysis(workday_record, linkid)
+    plt.plot(lowess_result[:,0],lowess_result[:,1], label='sequence={0},linkid={1}'.format(ind,linkid))
+plt.legend()
+plt.xlabel('time(s)')
+plt.ylabel('lowess speed')
+plt.title('workday base pattern')
 
 
 """
 def max_min(series):
     return series.max()-series.loc[series<0].max()
-engine = create_engine('mysql+mysqlconnector://root:@localhost/beijing_bus_liuliqiao', echo=False)
+engine = create_engine('mysql+mysqlconnector://root:a2=b2=c2@localhost/beijing_bus_liuliqiao', echo=False)
 cnx = engine.raw_connection()
 result_diff = pd.DataFrame(columns=['diff_max','count','max_pivot','line_id'])
 print('line includes', linenum_list)
