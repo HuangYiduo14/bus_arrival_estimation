ea903362681dcea4990181cae322f6e79789221c
yiduo <yiduo_huang@berkeley.edu>
2019-12-08 09:18:49 -0800

    lowess for speed ts 1

--- background_tse.py
+++ background_tse.py
@@ -57,43 +57,49 @@ speed_record = speed_record.loc[speed_record['time'].dt.time<pd.to_datetime('23:
 speed_record['tm'] = speed_record['time'].dt.time.astype(str)
 speed_record['tm'] = speed_record['tm'].str[0].astype(int)*10*3600 + speed_record['tm'].str[1].astype(int)*3600 +speed_record['tm'].str[3].astype(int)*10*60+speed_record['tm'].str[4].astype(int)*60
 
-speed_record['base_pattern'] = 0
-holiday_record = speed_record.loc[speed_record['time'].dt.day.isin(holiday_list['day'])]
-workday_record = speed_record.loc[~(speed_record['time'].dt.day.isin(holiday_list['day']))]
-
 from statsmodels.nonparametric.smoothers_lowess import lowess
-def one_link_analysis(record, linkid=35972):
+def one_link_analysis(record, linkid=35972,day=1):
     speed_link = record.loc[record['linkid']==linkid]
-
-    #plt.figure()
-    #for day in speed_link['time'].dt.date.unique():
-    #    speed_day = speed_link.loc[speed_link['time'].dt.date==day]
-    #    plt.scatter(speed_day['tm'], speed_day['speed'])
+    speed_link = speed_link.loc[speed_link['time'].dt.day==day]
     x = speed_link['tm'].values
     y = speed_link['speed']
     new = lowess(y,x,frac=0.15)
-
-
     return new
 
-plt.figure()
-for ind, linkid in enumerate(xsh_s2n2.LinkID.values):
-    lowess_result = one_link_analysis(holiday_record, linkid)
-    plt.plot(lowess_result[:,0],lowess_result[:,1], label='sequence={0},linkid={1}'.format(ind,linkid))
-plt.legend()
-plt.xlabel('time(s)')
-plt.ylabel('lowess speed')
-plt.title('holiday base pattern')
+result_diff = pd.read_csv('result_公主坟南_六里桥北里_diff.csv')
+result_diff.drop('Unnamed: 0',axis=1,inplace=True)
+result_diff.sort_values('max_pivot',inplace=True)
+result_diff['tm']=result_diff['max_pivot']%1000000
+result_diff['tm'] = (result_diff['tm']//100)%100*60 + (result_diff['tm']//10000)*60*60+result_diff['tm']%100
+result_diff['max_pivot'] = pd.to_datetime(result_diff['max_pivot'].astype(str))
+d_length_last = xsh_s2n1.iloc[-1,-2]-maxy
+d_length_first = miny -  xsh_s2n1.iloc[0,-1]
 
-plt.figure()
-for ind, linkid in enumerate(xsh_s2n2.LinkID.values):
-    lowess_result = one_link_analysis(workday_record, linkid)
-    plt.plot(lowess_result[:,0],lowess_result[:,1], label='sequence={0},linkid={1}'.format(ind,linkid))
-plt.legend()
-plt.xlabel('time(s)')
-plt.ylabel('lowess speed')
-plt.title('workday base pattern')
+new_result = pd.DataFrame(columns=['diff_max','count','max_pivot','line_id','tm']+['speed_{0}'.format(linkid) for linkid in xsh_s2n1['LinkID'].values])
+for day in range(1,30):
+    print(day,'-'*50)
+    result_day = result_diff.loc[result_diff['max_pivot'].dt.day==day]
+    for linkid in xsh_s2n1['LinkID'].values:
+        print(linkid)
+        low_result = pd.DataFrame(one_link_analysis(speed_record,linkid,day),columns=['tm','speed'])
+        low_result['tm'] = low_result['tm'].astype('int64')
+        result_day = pd.merge_asof(
+            result_day, low_result, on='tm'
+        )
+        result_day.rename(columns={'speed':'speed_{0}'.format(linkid)},inplace=True)
+    new_result = pd.concat([new_result,result_day])
 
+new_result['free_time'] = 0
+for ind in xsh_s2n1.index:
+    linkid = xsh_s2n1.loc[ind,'LinkID']
+    if xsh_s2n1.loc[ind,'min_y']<miny:
+        len_link = xsh_s2n1.loc[ind,'LENGTH'] - d_length_first
+    elif xsh_s2n1.loc[ind,'max_y']>maxy:
+        len_link = xsh_s2n1.loc[ind, 'LENGTH'] - d_length_last
+    else:
+        len_link = xsh_s2n1.loc[ind, 'LENGTH']
+    # here 36 is the ratio from km/h to m/s
+    new_result['free_time'] = new_result['free_time'] + 3.6*len_link/new_result['speed_{0}'.format(linkid)]
 
 """
 def max_min(series):
