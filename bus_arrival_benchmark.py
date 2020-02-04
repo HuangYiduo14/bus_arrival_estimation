6b6d43e1873c59b18fcb9bcd6038765b9b84c945
yiduo <yiduo_huang@berkeley.edu>
2020-02-04 15:14:08 -0800

    report 2019: trip identification new

--- bus_arrival_benchmark.py
+++ bus_arrival_benchmark.py
@@ -1,8 +1,13 @@
 import mysql.connector
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
+from pylab import mpl
+mpl.rcParams['font.sans-serif'] = ['SimHei']
 
+busstation = pd.read_csv(
+    'beijing_data/2014版北京路网_接点_六里桥区-2018/北京局部区域公交相关数据/北京局部区域公交相关数据/附件 刷卡数据与GIS对应规则/站点静态表.txt', encoding='gbk')
 # initialize sql connector
 print('getting sql data')
 cnx = mysql.connector.connect(user='root', password='a2=b2=c2', database='beijing_bus_liuliqiao')
@@ -61,17 +66,25 @@ def detect_round_info(df):
     # Greedy method is used to determine whether choose post direction or pre direction
     l_post_direction = l_new_station[1:] - l_new_station[:-1]
     l_pre_direction = np.insert(l_post_direction, 0, np.nan)
-    l_new_station_time = df_record.loc[l_new_station_ind, 'trans_time'].values
-    l_post_time_gap = l_new_station_time[1:] - l_new_station_time[:-1]
-    l_pre_time_gap = np.insert(l_post_time_gap, 0, 0)
-    l_skew_post = l_pre_time_gap > np.insert(l_post_time_gap, -1, 3600)
-    l_pre_direction[l_skew_post] = np.insert(l_post_direction, -1, np.nan)[l_skew_post]
-    df_record.loc[l_new_station_ind, 'direction'] = l_pre_direction
-    df_record['direction'] = df_record['direction'].clip(lower=-1, upper=1)
+    l_post_direction = np.insert(l_post_direction, -1, np.nan)
+    #l_new_station_time = df_record.loc[l_new_station_ind, 'trans_time'].values
+    #l_post_time_gap = l_new_station_time[1:] - l_new_station_time[:-1]
+    #l_pre_time_gap = np.insert(l_post_time_gap, 0, 3600)
+    # we need to split record lines (not only station)
+    #l_skew_post = l_pre_time_gap > np.insert(l_post_time_gap, -1, 3600)
+    #l_pre_direction[l_skew_post] = np.insert(l_post_direction, -1, np.nan)[l_skew_post]
+    df_record.loc[l_new_station_ind, 'prev_direction'] = l_pre_direction
+    df_record.loc[l_new_station_ind,'succ_direction'] = l_post_direction
+    df_record['time_diff'] = df_record['trans_time'] - df_record['trans_time'].shift(1)
+
+
     # Detect the round shift station by back-and-forth filling, denoted by 0
-    df_record['prev_direction'] = df_record['direction'].fillna(method='ffill').fillna(method='bfill')
-    df_record['succ_direction'] = df_record['direction'].fillna(method='bfill').fillna(method='ffill')
+    df_record['prev_direction'] = df_record['prev_direction'].fillna(method='ffill').fillna(method='bfill')
+    df_record['succ_direction'] = df_record['succ_direction'].fillna(method='ffill').fillna(method='bfill')
+    df_record['prev_direction'] = df_record['prev_direction'].clip(lower=-1, upper=1)
+    df_record['succ_direction'] = df_record['succ_direction'].clip(lower=-1, upper=1)
     df_record['direction'] = (df_record['prev_direction'] + df_record['succ_direction']) / 2
+    df_record['is_new_round'] = 0
     # Extract the exact first record before and after round shift
     l_before_change = df_record[(
                                         df_record['direction'].shift(-1) != 0) & (
@@ -79,6 +92,10 @@ def detect_round_info(df):
     l_after_change = df_record[(
                                        df_record['direction'].shift(1) == 0) & (
                                        df_record['direction'] != 0)].index.tolist()
+    if len(l_after_change)>len(l_before_change):
+        l_after_change = l_after_change[1:]
+    elif len(l_after_change)<len(l_before_change):
+        l_before_change = l_before_change[:-1]
     # Calculate time difference and the spliting time
     df_change = pd.DataFrame()
     df_change['before_time'] = df_record.loc[l_before_change, 'trans_time'].reset_index(drop=1)
@@ -91,13 +108,17 @@ def detect_round_info(df):
     df_record.loc[l_change.tolist(), 'direction'] = df_record.loc[
         df_change['after_index'].tolist(), 'direction'].tolist()
     df_record.loc[l_change.tolist(), 'is_new_station'] = 1
+
+    df_record.loc[(df_record['is_new_station']==1)&(df_record['prev_direction']!=df_record['succ_direction']),'is_new_round']=1
+    df_record.loc[df_record['time_diff']>3600, 'is_new_round']=1
+
+    df_record.loc[l_change.tolist(), 'is_new_round'] = 1
     df_record.loc[df_record['direction'] == 0, 'direction'] = np.nan
     df_record['direction'] = df_record['direction'].fillna(method='ffill')
 
     # Calibrate station number which is not consistent with direction
     l_need_cast = (df_record['start_station'] <= df_record['end_station']) * 2 - 1 != df_record['direction']
     df_record.loc[l_need_cast, 'start_station'] = df_record.loc[l_need_cast, 'end_station']
-    df_record['is_new_round'] = (df_record['direction'].shift(1) != df_record['direction']).astype(int)
     #df_record.to_csv('temp2.csv')
     return df_record.drop(['prev_direction', 'succ_direction'], axis=1)
 
@@ -108,26 +129,39 @@ def merge_one_round(df_round):
     print('start round merging')
     round_info = df_round.groupby('round_id').agg({'direction':'mean','trans_time':['min','max'],'end_station':['min','max', pd.Series.nunique]})
     round_info.columns = ['_'.join(i) for i in round_info.columns]
-    round_info['same_prev_direction'] = round_info['direction_mean'] != round_info['direction_mean'].shift(1)
-    round_info['same_next_direction'] = round_info['direction_mean'] != round_info['direction_mean'].shift(-1)
-    small_round_ind = round_info.loc[round_info['end_station_nunique']<=10]
+    small_round_ind = round_info.loc[round_info['end_station_nunique']<=3].index
+    #round_info = round_info.loc[round_info['end_station_nunique'] >= 4]
+    def common_station(df_round, trip1,trip2):
+        stations1 = set(df_round.loc[df_round['round_id']==trip1,'end_station'])
+        stations2 = set(df_round.loc[df_round['round_id']==trip2,'end_station'])
+        return len(stations1&stations2)
     def merge2(df_round, trip1, trip2):
         # merge trip2(smaller) into trip1
         s1 = df_round.loc[df_round['round_id']==trip1, 'direction']
         direction1 = s1.iloc[0]
-        df_round.loc[df_round['round_id'] == trip2, 'direction'] = direction1
+        df_round.loc[df_round['round_id'] == trip2,'direction'] = direction1
         df_round.loc[df_round['round_id'] == trip2,'round_id'] = trip1
-
-    for ind in small_round_ind:
-        if round_info.loc[ind,'same_prev_direction']:
-
-
-    # 3.2. merge 'big trip' round
-
-
-
-
-    return round_info
+        return df_round
+    round_number = round_info.shape[0]
+    ind = 1
+    while ind <= round_number-2:
+        if ind in small_round_ind:
+            ind += 1
+            continue
+        ind2 = ind + 1
+        while True:
+            ind3 = ind2+1
+            if (ind2 in small_round_ind) \
+                    and (common_station(df_round,ind,ind3)<=3
+                                              and (round_info.loc[ind,'direction_mean']==round_info.loc[ind3,'direction_mean']
+                                                   and np.abs(round_info.loc[ind,'trans_time_max']-round_info.loc[ind3,'trans_time_min'])<20*60)) :
+                df_round=merge2(df_round, ind, ind2)
+                df_round=merge2(df_round, ind, ind3)
+            else:
+                ind = ind2
+                break
+            ind2+=2
+    return df_round
 
 
 
@@ -156,37 +190,6 @@ def passenger_num_count(df, max_station):
 
     df_pax_num = df_record.groupby('round_id').apply(lambda x: cal_pax_num(x, max_station))
     return df_pax_num
-
-
-class Round:
-    def __init__(self, bus_id, direction, i_val_round, j_val_round, arrival_time_round, number_passenger_round):
-        self.bus_id = bus_id
-        self.direction = direction
-        self.i_val_round = i_val_round  # total number of passengers alighting at this station
-        self.j_val_round = j_val_round  # total number of aggregated passengers
-        self.arrival_time_round = np.nan_to_num(arrival_time_round)  # estimated arrival time
-        self.number_passenger_round = number_passenger_round  # total of passenger on board at this station
-        self.round_last_time = self.arrival_time_round.max()  # the last valid estimated arrival
-        # we will also use
-        # 1. weekdays
-        # 2. weather? holidays?.....
-        # as known features
-
-    def __lt__(self, other):
-        if self.direction < other.direction:
-            return True
-        elif self.round_last_time < other.round_last_time:
-            return True
-        return False
-
-    def __str__(self):
-        s0 = """
-        bus_id = {self.bus_id}
-        direction = {self.direction}
-        arrival_time_estimated = {self.arrival_time_round}
-        """.format(self=self)
-        return s0
-
 
 def estimate_arrival_time_local(df_round, df_pax_num):
     # setting the time range for outliers and record clustering
@@ -260,19 +263,29 @@ def matrix_with_missing_construction(round_info):
     for i in range(row_num):
         M[i] = round_info[i].arrival_time_round
     return M
+def line_station2chinese(line_id, station_id):
+    if line_id in busstation['linenum']:
+        line_info = busstation.loc[busstation['linenum'] == line_id]
+        if sum(line_info['num']==station_id)>0:
+            name = line_info.loc[line_info['num']==station_id,'stationname'].values[0]
+        else:
+            name=station_id
+    else:
+        name=station_id
+    return name
 
 def time2int(time):
     return time//10000*3600+(time%10000)//100*60+time%100
 def int2timedate(time_s):
     day = time_s//(24*3600)
-    day = min(day, 30)
-    time_s -= time_s%(24*3600)
+    day = 1+min(day, 30)
+    time_s = time_s%(24*3600)
     hour = time_s//3600
     time_s = time_s%3600
-    min = time_s//60
+    minute = time_s//60
     time_s = time_s%60
     sec = time_s
-    return pd.to_datetime('2018-06-{0} {1}:{2}:{3}'.format(day,hour,min,sec))
+    return pd.to_datetime('2018-06-{0} {1}:{2}:{3}'.format(day,hour,minute,sec))
 
 # preprocess time data: convert yyyymmdd HHMMSS to integer: seconds from 20180601 00:00:00
 line57_record['trans_time'] = (line57_record['trans_date'] - 20180601) * 24 * 3600 + line57_record['trans_time'].apply(time2int)
@@ -288,21 +301,68 @@ line57_record = line57_record.drop_duplicates()
 line57_record['start_station'] = line57_record['start_station'].clip(lower=1, upper=max_station)
 line57_record['end_station'] = line57_record['end_station'].clip(lower=1, upper=max_station)
 # testing: one bus
-i=0
-line57_onebus = line57_record.loc[line57_record['bus_unique'] == i]
-line57_onebus_temp = line57_onebus.sort_values('trans_time').drop(['bus_id', 'bus_unique'], axis=1)
-df_station = aggregate_record_station(line57_onebus_temp)
-df_round=detect_round_info(df_station)
-df_round['round_id'] = df_round['is_new_round'].cumsum()
-import matplotlib.pyplot as plt
-for i in df_round['round_id'].unique():
-    trip_info = df_round.loc[df_round['round_id']==i]
-    plt.scatter(trip_info['trans_time'], trip_info['end_station'])
-    plt.text(trip_info['trans_time'].iloc[0], trip_info['end_station'].iloc[0], trip_info['direction'].iloc[0])
+#for i in [0]:
+for i in line57_record['bus_unique'].unique():
+    print('start analysis of bus {0}, bus id{1}'.format(i, le.classes_[i]), '--' * 50)
+    line57_onebus = line57_record.loc[line57_record['bus_unique'] == i]
+    line57_onebus_temp = line57_onebus.sort_values('trans_time').drop(['bus_id', 'bus_unique'], axis=1)
+    if line57_onebus.shape[0]<10:
+        continue
+    df_station = aggregate_record_station(line57_onebus_temp)
+    df_round = detect_round_info(df_station)
+    df_round['round_id'] = df_round['is_new_round'].cumsum()
+    df_round = merge_one_round(df_round)
+    df_round[['start_station', 'end_station']] = df_round[['start_station', 'end_station']].astype(int)
+    #df_passenger_number = passenger_num_count(df_round, max_station)
+    #df_arrival_time, df_i_val, df_j_val = estimate_arrival_time_local(df_round, df_passenger_number)
+    chinese_station_name =[line_station2chinese(line_id, k) for k in np.sort(df_round['end_station'].unique())]
+    for j in df_round['round_id'].unique():
+        trip_info = df_round.loc[df_round['round_id'] == j]
+        trip_info.sort_values(by=['end_station', 'trans_time'])
+        trip_info = trip_info.loc[trip_info['trans_time']<48*3600]
+        if trip_info.shape[0]<10 or trip_info['direction'].mean()<=0:
+           continue
+        plt.scatter(trip_info['trans_time'].apply(int2timedate).to_list() + trip_info['start_time'].apply(int2timedate).to_list(),
+                    trip_info['end_station'].to_list() + trip_info['start_station'].to_list(), alpha=0.2)
+        plt.plot(trip_info['trans_time'].apply(int2timedate).to_list(), trip_info['end_station'])
+        plt.yticks(np.sort(df_round['end_station'].unique()).tolist(), chinese_station_name)
+        #plt.text(trip_info['trans_time'].apply(int2timedate).iloc[0], trip_info['end_station'].iloc[0]+0.01, trip_info['direction'].mean())
+        #plt.text(trip_info['trans_time'].apply(int2timedate).iloc[-1], trip_info['end_station'].iloc[-1]+0.01, le.classes_[i])
+
+
 
 
 
 """
+class Round:
+    def __init__(self, bus_id, direction, i_val_round, j_val_round, arrival_time_round, number_passenger_round):
+        self.bus_id = bus_id
+        self.direction = direction
+        self.i_val_round = i_val_round  # total number of passengers alighting at this station
+        self.j_val_round = j_val_round  # total number of aggregated passengers
+        self.arrival_time_round = np.nan_to_num(arrival_time_round)  # estimated arrival time
+        self.number_passenger_round = number_passenger_round  # total of passenger on board at this station
+        self.round_last_time = self.arrival_time_round.max()  # the last valid estimated arrival
+        # we will also use
+        # 1. weekdays
+        # 2. weather? holidays?.....
+        # as known features
+
+    def __lt__(self, other):
+        if self.direction < other.direction:
+            return True
+        elif self.round_last_time < other.round_last_time:
+            return True
+        return False
+
+    def __str__(self):
+        s0 = '''
+        bus_id = {self.bus_id}
+        direction = {self.direction}
+        arrival_time_estimated = {self.arrival_time_round}
+        '''.format(self=self)
+        return s0
+        
 forward_round_info = []
 backward_round_info = []
 for i in range(1):
